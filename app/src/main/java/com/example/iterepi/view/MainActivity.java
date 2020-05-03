178b015bb45d4f7fbe5173367d167c75e8a62514
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-05-03 06:19:11 -0500

    Change color scheme and added launch screen layout

--- app/src/main/java/com/example/iterepi/view/MainActivity.java
+++ app/src/main/java/com/example/iterepi/view/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.iterepi.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.iterepi.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView logoIV;
+    private ImageView nameIV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launch_screen);
+
+        logoIV = findViewById(R.id.logoIV);
+        nameIV = findViewById(R.id.nameIV);
     }
 }
