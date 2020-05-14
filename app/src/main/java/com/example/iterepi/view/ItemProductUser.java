85f152a7e91dfca4da449e5cf1951453090c3313
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-05-13 23:37:20 -0500

    Add ItemProductUser class

--- app/src/main/java/com/example/iterepi/view/ItemProductUser.java
+++ app/src/main/java/com/example/iterepi/view/ItemProductUser.java
@@ -3,14 +3,49 @@ package com.example.iterepi.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.iterepi.R;
 
 public class ItemProductUser extends AppCompatActivity {
 
+
+    private TextView showNameProductTV;
+    private TextView showPriceTV;
+    private TextView showQualityTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_product_user);
+
+        showNameProductTV = findViewById(R.id.showNameProductTV);
+        showPriceTV = findViewById(R.id.showPriceTV);
+        showQualityTV = findViewById(R.id.showQualityTV);
+
+    }
+
+    public TextView getShowNameProductTV() {
+        return showNameProductTV;
+    }
+
+    public void setShowNameProductTV(TextView showNameProductTV) {
+        this.showNameProductTV = showNameProductTV;
+    }
+
+    public TextView getShowPriceTV() {
+        return showPriceTV;
+    }
+
+    public void setShowPriceTV(TextView showPriceTV) {
+        this.showPriceTV = showPriceTV;
+    }
+
+    public TextView getShowQualityTV() {
+        return showQualityTV;
+    }
+
+    public void setShowQualityTV(TextView showQualityTV) {
+        this.showQualityTV = showQualityTV;
     }
 }
