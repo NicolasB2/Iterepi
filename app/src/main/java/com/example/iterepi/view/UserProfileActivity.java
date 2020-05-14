5d723a2db15ce212989ec2b870ab3e6af9a446f4
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-05-13 23:42:30 -0500

    add user profile class

--- app/src/main/java/com/example/iterepi/view/UserProfileActivity.java
+++ app/src/main/java/com/example/iterepi/view/UserProfileActivity.java
@@ -3,14 +3,90 @@ package com.example.iterepi.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.iterepi.R;
 
 public class UserProfileActivity extends AppCompatActivity {
 
+    private ImageView profileImage;
+    private ImageButton backBtn;
+    private ImageButton setImageBtn;
+
+    private TextView nameTV;
+    private TextView BirthTV;
+    private TextView genderTV;
+    private TextView idTV;
+    private TextView phoneNumberTV;
+    private TextView emailTV;
+
+    private Button changePassBtn;
+    private Button update_dataBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        profileImage = findViewById(R.id.profileImage);
+        backBtn  = findViewById(R.id.backBtn);
+        setImageBtn  = findViewById(R.id.setImageBtn);
+
+        nameTV  = findViewById(R.id.nameTV);
+        BirthTV  = findViewById(R.id.BirthTV);
+        genderTV  = findViewById(R.id.genderTV);
+        idTV  = findViewById(R.id.idTV);
+        phoneNumberTV  = findViewById(R.id.phoneNumberTV);
+        emailTV  = findViewById(R.id.emailTV);
+
+        changePassBtn  = findViewById(R.id.changePassBtn);
+        update_dataBtn  = findViewById(R.id.update_dataBtn);
+    }
+
+    public ImageView getProfileImage() {
+        return profileImage;
+    }
+
+    public ImageButton getBackBtn() {
+        return backBtn;
+    }
+
+    public ImageButton getSetImageBtn() {
+        return setImageBtn;
+    }
+
+    public TextView getNameTV() {
+        return nameTV;
+    }
+
+    public TextView getBirthTV() {
+        return BirthTV;
+    }
+
+    public TextView getGenderTV() {
+        return genderTV;
+    }
+
+    public TextView getIdTV() {
+        return idTV;
+    }
+
+    public TextView getPhoneNumberTV() {
+        return phoneNumberTV;
+    }
+
+    public TextView getEmailTV() {
+        return emailTV;
+    }
+
+    public Button getChangePassBtn() {
+        return changePassBtn;
+    }
+
+    public Button getUpdate_dataBtn() {
+        return update_dataBtn;
     }
 }
