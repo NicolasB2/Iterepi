66974d22d1de51a2df882f2a2fe3dfa0a89bada8
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-05-16 17:25:09 -0500

    Add SectionStoreUser class and change of listview by recyclerview

--- app/src/main/java/com/example/iterepi/view/SectionStoreUser.java
+++ app/src/main/java/com/example/iterepi/view/SectionStoreUser.java
@@ -1,16 +1,63 @@
 package com.example.iterepi.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.iterepi.R;
 
 public class SectionStoreUser extends AppCompatActivity {
 
+    private TextView nameStoreTV;
+    private ImageButton backBtn;
+    private ImageView imageLogoStoreIV;
+    private RecyclerView listProductsRV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section_store_user);
+
+        nameStoreTV = findViewById(R.id.nameStoreTV);
+        backBtn = findViewById(R.id.backBtn);
+        imageLogoStoreIV = findViewById(R.id.imageLogoStoreIV);
+        listProductsRV = findViewById(R.id.listProductsRV);
+
+    }
+
+    public TextView getNameStoreTV() {
+        return nameStoreTV;
+    }
+
+    public void setNameStoreTV(TextView nameStoreTV) {
+        this.nameStoreTV = nameStoreTV;
+    }
+
+    public ImageButton getBackBtn() {
+        return backBtn;
+    }
+
+    public void setBackBtn(ImageButton backBtn) {
+        this.backBtn = backBtn;
+    }
+
+    public ImageView getImageLogoStoreIV() {
+        return imageLogoStoreIV;
+    }
+
+    public void setImageLogoStoreIV(ImageView imageLogoStoreIV) {
+        this.imageLogoStoreIV = imageLogoStoreIV;
+    }
+
+    public RecyclerView getListProductsRV() {
+        return listProductsRV;
+    }
+
+    public void setListProductsRV(RecyclerView listProductsRV) {
+        this.listProductsRV = listProductsRV;
     }
 }
