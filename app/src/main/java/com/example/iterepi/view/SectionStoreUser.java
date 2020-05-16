a31deebc90a3fc417207e80c3170de029d73e0c9
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-05-16 16:51:11 -0500

    Add layout section store user

---
+++ app/src/main/java/com/example/iterepi/view/SectionStoreUser.java
@@ -0,0 +1,16 @@
+package com.example.iterepi.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.iterepi.R;
+
+public class SectionStoreUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_store_user);
+    }
+}
