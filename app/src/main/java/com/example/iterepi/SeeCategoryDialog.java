cb59d0fb649b8bfcb079818ded14d411c154cf08
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-05-21 00:13:10 -0500

    Add see category dialog

---
+++ app/src/main/java/com/example/iterepi/SeeCategoryDialog.java
@@ -0,0 +1,14 @@
+package com.example.iterepi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeeCategoryDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_see_category);
+    }
+}
--- app/src/main/java/com/example/iterepi/view/SeeProductDialog.java
+++ app/src/main/java/com/example/iterepi/view/SeeProductDialog.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.iterepi.R;
 
-public class seeCategoryDialog extends AppCompatActivity {
+public class SeeProductDialog extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dialog_see_category);
+        setContentView(R.layout.dialog_see_product);
     }
 }
