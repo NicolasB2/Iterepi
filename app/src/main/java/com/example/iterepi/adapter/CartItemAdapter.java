c3f49df73640989defac3a15179c05fdaf3c1acd
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-05-21 17:47:07 -0500

    Cart Fragment with RecyclerView logic

---
+++ app/src/main/java/com/example/iterepi/adapter/CartItemAdapter.java
@@ -0,0 +1,90 @@
+package com.example.iterepi.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.iterepi.R;
+import com.example.iterepi.model.Item;
+
+
+import java.util.ArrayList;
+
+public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.ViewHolder> implements View.OnClickListener {
+
+    private ArrayList<Item> cartItems;
+
+    //Listener
+    private View.OnClickListener listener;
+
+    public CartItemAdapter(ArrayList<Item> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cart_product, parent, false);
+        view.setOnClickListener(this);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        String nameProduct = cartItems.get(position).getName();
+        String price = "Precio: " + cartItems.get(position).getPrice();
+        String quantity = "Cantidad: " + cartItems.get(position).getQuantity();
+        //TODO: Image missing
+
+        holder.nameProductTV.setText(nameProduct);
+        holder.priceTV.setText(price);
+        holder.quantityTV.setText(quantity);
+        //TODO: Image missing
+    }
+
+    @Override
+    public int getItemCount() {
+        return cartItems.size();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(listener != null){
+            listener.onClick(v);
+        }
+    }
+
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener = listener;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView nameProductTV, priceTV, quantityTV;
+        private ImageButton closeBtn, plusBtn, minusBtn;
+        private ImageView productIV;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nameProductTV = itemView.findViewById(R.id.nameProductTV);
+            priceTV = itemView.findViewById(R.id.priceTV);
+            quantityTV = itemView.findViewById(R.id.quantityTV);
+            closeBtn = itemView.findViewById(R.id.closeBtn);
+            plusBtn = itemView.findViewById(R.id.plusBtn);
+            minusBtn = itemView.findViewById(R.id.minusBtn);
+            productIV = itemView.findViewById(R.id.productIV);
+
+        }
+    }
+}
--- app/src/main/java/com/example/iterepi/view/user/CartFragment.java
+++ app/src/main/java/com/example/iterepi/view/user/CartFragment.java
@@ -8,17 +8,48 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.iterepi.R;
+import com.example.iterepi.adapter.CartItemAdapter;
+import com.example.iterepi.controller.CartController;
+import com.example.iterepi.model.Item;
+
+import java.util.ArrayList;
 
 public class CartFragment extends Fragment {
 
     private View view;
+    private CartItemAdapter cartItemAdapter;
+    private ArrayList<Item> cartItems;
+    private RecyclerView listCartRV;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_cart,container,false);
+        view = inflater.inflate(R.layout.fragment_cart, container, false);
+        listCartRV = view.findViewById(R.id.listCartRV);
+        listCartRV.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        loadItems();
+        cartItemAdapter = new CartItemAdapter(cartItems);
+        listCartRV.setAdapter(cartItemAdapter);
         return view;
+    }
+
+    public void loadItems() {
+
+        cartItems = new ArrayList<>();
+        
+        //TODO: load item list from DB
+
+        /* For testing
+        cartItems.add(new Item("id1", "name", "description", 10.00, 1, "photo"));
+        cartItems.add(new Item("id2", "name", "description", 20.00, 2, "photo"));
+        cartItems.add(new Item("id3", "name", "description", 30.00, 3, "photo"));
+        cartItems.add(new Item("id4", "name", "description", 40.00, 4, "photo"));
+        cartItems.add(new Item("id5", "name", "description", 50.00, 5, "photo"));
+        cartItems.add(new Item("id6", "name", "description", 60.00, 6, "photo"));*/
     }
 }
