fb4bcd4c7878fac4558f9ce4488b1972e2690e35
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-05-22 14:31:45 -0500

    Add atributes in seeCategoryDialog class

--- app/src/main/java/com/example/iterepi/view/store/SeeCategoryDialog.java
+++ app/src/main/java/com/example/iterepi/view/store/SeeCategoryDialog.java
@@ -1,16 +1,42 @@
 package com.example.iterepi.view.store;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.iterepi.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SeeCategoryDialog extends AppCompatActivity {
+
+    private ImageButton backBtn3;
+    private TextView categoryName;
+    private ImageView purchaseImageIV;
+    private TextView purchaseNameTV;
+    private Spinner placeNameSpinner;
+    private Spinner productsSpinner;
+    private ListView listItems;
+    private Button updateDataBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_see_category);
+
+        backBtn3 = findViewById(R.id.backBtn3);
+        categoryName = findViewById(R.id.categoryName);
+        purchaseImageIV = findViewById(R.id.purchaseImageIV);
+        purchaseNameTV = findViewById(R.id.purchaseNameTV);
+        placeNameSpinner = findViewById(R.id.placeNameSpinner);
+        productsSpinner = findViewById(R.id.productsSpinner);
+        listItems = findViewById(R.id.listItems);
+        updateDataBtn = findViewById(R.id.updateDataBtn);
     }
 }
