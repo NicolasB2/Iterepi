bbb6498021add40ccc83d7faae5122e911f9adbe
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-05-24 16:19:39 -0500

    Add AddPaymentMethod class

--- app/src/main/java/com/example/iterepi/view/user/AddPaymentMethod.java
+++ app/src/main/java/com/example/iterepi/view/user/AddPaymentMethod.java
@@ -3,14 +3,94 @@ package com.example.iterepi.view.user;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.iterepi.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AddPaymentMethod extends AppCompatActivity {
+
+    private ImageButton backBtn;
+
+    private TextInputLayout cardNumberTF;
+    private TextInputLayout expirationDate;
+    private TextInputLayout securityCodTF;
+    private TextInputLayout nameTF;
+    private TextInputLayout lastNameTF;
+
+    private Button addCardBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_payment_method);
+
+        backBtn = findViewById(R.id.backBtn);
+
+        cardNumberTF = findViewById(R.id.cardNumberTF);
+        expirationDate = findViewById(R.id.expirationDateTF);
+        securityCodTF = findViewById(R.id.securityCodTF);
+        nameTF = findViewById(R.id.nameTF);
+        lastNameTF = findViewById(R.id.lastNameTF);
+
+        addCardBtn = findViewById(R.id.addCardBtn);
+
+    }
+
+    public ImageButton getBackBtn() {
+        return backBtn;
+    }
+
+    public void setBackBtn(ImageButton backBtn) {
+        this.backBtn = backBtn;
+    }
+
+    public TextInputLayout getCardNumberTF() {
+        return cardNumberTF;
+    }
+
+    public void setCardNumberTF(TextInputLayout cardNumberTF) {
+        this.cardNumberTF = cardNumberTF;
+    }
+
+    public TextInputLayout getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(TextInputLayout expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public TextInputLayout getSecurityCodTF() {
+        return securityCodTF;
+    }
+
+    public void setSecurityCodTF(TextInputLayout securityCodTF) {
+        this.securityCodTF = securityCodTF;
+    }
+
+    public TextInputLayout getNameTF() {
+        return nameTF;
+    }
+
+    public void setNameTF(TextInputLayout nameTF) {
+        this.nameTF = nameTF;
+    }
+
+    public TextInputLayout getLastNameTF() {
+        return lastNameTF;
+    }
+
+    public void setLastNameTF(TextInputLayout lastNameTF) {
+        this.lastNameTF = lastNameTF;
+    }
+
+    public Button getAddCardBtn() {
+        return addCardBtn;
+    }
+
+    public void setAddCardBtn(Button addCardBtn) {
+        this.addCardBtn = addCardBtn;
     }
 }
