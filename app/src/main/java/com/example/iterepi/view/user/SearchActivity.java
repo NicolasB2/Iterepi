3ac108c2f99aa1d620b88bd350f38452bbc18b9d
juanchovelezpro <velezolaya2012@hotmail.com>
2020-05-27 09:17:23 -0500

    Fixed Buttom Navigation View with events and selected item.

--- app/src/main/java/com/example/iterepi/view/user/SearchActivity.java
+++ app/src/main/java/com/example/iterepi/view/user/SearchActivity.java
@@ -1,14 +1,10 @@
 package com.example.iterepi.view.user;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-
 import com.example.iterepi.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -17,29 +13,5 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
-        bottomNavigationView.setSelectedItemId(R.id.search);
-
-        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
-            @Override
-            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.feed:
-                        startActivity(new Intent(getApplicationContext(), UserFeedActivity.class));
-                        finish();
-                        overridePendingTransition(0, 0);
-                        break;
-
-                    case R.id.location:
-
-                        startActivity(new Intent(getApplicationContext(), CartActivity.class));
-                        finish();
-                        overridePendingTransition(0, 0);
-                        break;
-                    case R.id.search:
-                        break;
-                }
-            }
-        });
     }
 }
